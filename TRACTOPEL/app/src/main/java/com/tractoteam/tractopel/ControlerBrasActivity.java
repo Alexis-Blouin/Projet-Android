4af73a1c9748edd55aaa4fa6b2b1925982c530f9
Mathis BEAL <mathis.beal@etu.uca.fr>
2024-03-25 15:31:39 -0400

    Fini layout de TestConnexionActivity, et possibilité de lancer les activités de contrôle

---
+++ TRACTOPEL/app/src/main/java/com/tractoteam/tractopel/ControlerBrasActivity.java
@@ -0,0 +1,58 @@
+package com.tractoteam.tractopel;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class ControlerBrasActivity extends AppCompatActivity {
+
+    String TAG = this.getClass().getName();
+
+    Toolbar m_toolbar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_controler_plateforme);
+
+        Log.d(TAG, "onCreate() from "+TAG);
+
+        //Ajout de m_toolbar
+        m_toolbar = (Toolbar) findViewById(R.id.toolbar);
+        m_toolbar.setTitle("");
+        setSupportActionBar(m_toolbar);
+        m_toolbar.setTitle(R.string.titre_activite_controle_bras);
+        m_toolbar.setTitleTextColor(Color.WHITE);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.boutonChangerControles :
+                startActivity(new Intent(this, ControlerPlateformeActivity.class));
+                break;
+            case R.id.boutonRetourAuTest :
+                startActivity(new Intent(this, TestConnexionActivity.class));
+                break;
+        }
+        return true;
+    }
+}
--- TRACTOPEL/app/src/main/java/com/tractoteam/tractopel/TestConnexionActivity.java
+++ TRACTOPEL/app/src/main/java/com/tractoteam/tractopel/TestConnexionActivity.java
@@ -3,21 +3,28 @@ package com.tractoteam.tractopel;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class TestConnexionActivity extends AppCompatActivity {
 
-    String TAG = TestConnexionActivity.class.getName();
+    String TAG = this.getClass().getName();
 
     Toolbar m_toolbar;
 
+    TextView resultatTestConnectionTextview;
+
     Button lancerTestBouton;
+    Button boutonControlerPlateforme;
+    Button boutonControlerBras;
+
+    Intent intentControlerBrasActivity;
+    Intent intentControlerPlateformeActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,19 +33,50 @@ public class TestConnexionActivity extends AppCompatActivity {
 
         Log.d(TAG, "onCreate() from "+TAG);
 
-        m_toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        resultatTestConnectionTextview = (TextView) findViewById(R.id.resultatTestConnectionTextview);
+
         lancerTestBouton = (Button) findViewById(R.id.testConnexionBouton);
+        boutonControlerPlateforme = (Button) findViewById(R.id.controlerPlateforme);
+        boutonControlerBras = (Button) findViewById(R.id.controlerBras);
+
+
+        // Initialisation des intents
+        intentControlerPlateformeActivity = new Intent(this, ControlerPlateformeActivity.class);
+        intentControlerBrasActivity = new Intent(this, ControlerBrasActivity.class);
 
 
         //Ajout de m_toolbar
+        m_toolbar = (Toolbar) findViewById(R.id.toolbar);
         m_toolbar.setTitle("");
         setSupportActionBar(m_toolbar);
-        m_toolbar.setTitle(R.string.test_connexion_titre_activite);
+        m_toolbar.setTitle(R.string.titre_activite_test_connexion);
         m_toolbar.setTitleTextColor(Color.WHITE);
     }
 
     public void LancerTestConnection(View view) {
+        boolean resultatTest = false;
 
+
+        if (resultatTest){
+            resultatTestConnectionTextview.setText(R.string.test_connection_reussi);
+            resultatTestConnectionTextview.setTextColor(Color.DKGRAY);
+        }
+        else {
+            resultatTestConnectionTextview.setText(R.string.test_connexion_echoue);
+            resultatTestConnectionTextview.setTextColor(Color.RED);
+        }
+
+        boutonControlerBras.setEnabled(true);
+        boutonControlerPlateforme.setEnabled(true);
+    }
+
+    public void LancerControlePlateforme(View view) {
+        startActivity(intentControlerPlateformeActivity);
+    }
+
+    public void LancerControleBras(View view) {
+        startActivity(intentControlerBrasActivity);
     }
 
 /*
