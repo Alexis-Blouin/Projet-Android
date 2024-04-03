f999263dd93796b4c7fe8edfe6247a9719aebee8
Mathis BEAL <chasseurfurtif43@gmail.com>
2024-04-03 15:43:56 -0400

    Détection des ACTION_DOWN et ACTION_UP sur les boutons

--- TRACTOPEL/app/src/main/java/com/tractoteam/tractopel/ControlerPlateformeActivity.java
+++ TRACTOPEL/app/src/main/java/com/tractoteam/tractopel/ControlerPlateformeActivity.java
@@ -11,12 +11,20 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
 public class ControlerPlateformeActivity extends AppCompatActivity {
 
     String TAG = this.getClass().getName();
 
     Toolbar m_toolbar;
+
+    Button boutonDroite;
+    Button boutonGauche;
+    Button boutonHaut;
+    Button boutonBas;
 
 
     @Override
@@ -26,6 +34,15 @@ public class ControlerPlateformeActivity extends AppCompatActivity {
 
         Log.d(TAG, "onCreate() from "+TAG);
 
+        boutonDroite = (Button) findViewById(R.id.boutonDroite);
+        boutonDroite.setOnTouchListener(DirectionButtonsListener);
+        boutonGauche = (Button) findViewById(R.id.boutonGauche);
+        boutonGauche.setOnTouchListener(DirectionButtonsListener);
+        boutonHaut = (Button) findViewById(R.id.boutonHaut);
+        boutonHaut.setOnTouchListener(DirectionButtonsListener);
+        boutonBas = (Button) findViewById(R.id.boutonBas);
+        boutonBas.setOnTouchListener(DirectionButtonsListener);
+
         //Ajout de m_toolbar
         m_toolbar = (Toolbar) findViewById(R.id.toolbar);
         m_toolbar.setTitle("");
@@ -33,6 +50,7 @@ public class ControlerPlateformeActivity extends AppCompatActivity {
         m_toolbar.setTitle(R.string.titre_activite_controle_plateforme);
         m_toolbar.setTitleTextColor(Color.WHITE);
     }
+
 
 
     @Override
@@ -56,4 +74,42 @@ public class ControlerPlateformeActivity extends AppCompatActivity {
         }
         return true;
     }
+
+
+    public void buttonRightPressed(View view) {
+        if (view.isPressed())
+        {
+            Log.d(TAG, "Button right is pressed");
+        }
+        if (view.isFocused())
+        {
+            Log.d(TAG, "Button right is focused");
+        }
+        if (view.isHovered())
+        {
+            Log.d(TAG, "Button right is hovered");
+        }
+    }
+
+
+
+    View.OnTouchListener DirectionButtonsListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+
+            String id = v.getResources().getResourceEntryName(v.getId());
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    Log.d(TAG, "Bouton " + id + " : ACTION_DOWN");
+                    break;
+                case MotionEvent.ACTION_UP:
+                    Log.d(TAG, "Bouton " + id + " : ACTION_UP");
+                    break;
+                default:
+                    Log.d(TAG, "WARNING : Unhandled action : Bouton " + id);
+                    break;
+            }
+            return true;
+        }
+    };
 }
